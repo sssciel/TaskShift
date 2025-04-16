ac24fa5df65aee4b8454d16b55b68145cc9d210f
damn <refordtube@gmail.com>
2025-04-16 20:58:01 +0300

    Improved forecaster interface

--- source/forecaster/interface.py
+++ source/forecaster/interface.py
@@ -5,33 +5,27 @@ FORECASTS_IN_ONE_DAY = 96
 
 model = ForecastModel()
 
+def get_avg_window(start=0, end=None, df_list = []):
+    if not df_list:
+        df_list = list(model.get_forecasts())
+
+    avg_list = []
+
+    for df in df_list:
+        if end is None:
+            end = len(df)
+
+        avg_list.append(np.mean(df[start:end]))
+
+    return avg_list
 
 def get_saturday_avg():
-    forecasts_cpu, forecasts_gpu = model.get_forecasts()
-
-    saturday_avg_cpu = np.mean(forecasts_cpu[:FORECASTS_IN_ONE_DAY])
-    saturday_avg_gpu = np.mean(forecasts_gpu[:FORECASTS_IN_ONE_DAY])
-
-    return saturday_avg_cpu, saturday_avg_gpu
+    return get_avg_window(end=FORECASTS_IN_ONE_DAY)
 
 
 def get_sunday_avg():
-    forecasts_cpu, forecasts_gpu = model.get_forecasts()
-
-    sunday_avg_cpu = np.mean(
-        forecasts_cpu[FORECASTS_IN_ONE_DAY : FORECASTS_IN_ONE_DAY * 2]
-    )
-    sunday_avg_gpu = np.mean(
-        forecasts_gpu[FORECASTS_IN_ONE_DAY : FORECASTS_IN_ONE_DAY * 2]
-    )
-
-    return sunday_avg_cpu, sunday_avg_gpu
+    return get_avg_window(start=FORECASTS_IN_ONE_DAY, end=FORECASTS_IN_ONE_DAY * 2)
 
 
 def get_weekend_avg():
-    forecasts_cpu, forecasts_gpu = model.get_forecasts()
-
-    weekend_avg_cpu = np.mean(forecasts_cpu)
-    weekend_avg_gpu = np.mean(forecasts_gpu)
-
-    return weekend_avg_cpu, weekend_avg_gpu
+    return get_avg_window()
